a0137ea67cd5b2427c349354552a359cd3eeb4ad
Ettore <ettorecelozzi@gmail.com>
2017-05-07 14:11:09 +0200

    aggiornamento

--- exp.py
+++ exp.py
@@ -1,77 +1,62 @@
 import pickle
+import Test
 import matplotlib.pyplot as plt
-import test
 
-'''
-x = np.arange(0,3*np.pi,0.1)
-y = np.sin(x)
-plt.plot(x,y)
-plt.show()
-'''
-perc = (10, 20, 30, 40, 50, 60, 70, 75, 80, 85, 90, 91, 92, 93, 94, 95, 96, 97, 98, 100)
 m = 1000
-val = [m, perc]
+perc = (10, 20, 30, 40, 50, 60, 70, 75, 80, 85, 87, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100)
+val = (m, perc)
+
 pickle.dump(val, open("percents.p", "wb"))
 
-test.test()
+Test.test()
+
 chainedcol = pickle.load(open("chained.p", "rb"))
 iaresult = pickle.load(open("iaopen.p", "rb"))
-x = perc
-yMax = []
-for i in range(0, len(chainedcol), 3):
-    yMax.append(chainedcol[i])
-yMin = []
-for i in range(1, len(chainedcol), 3):
-    yMin.append(chainedcol[i])
-yAvg = []
-for i in range(2, len(chainedcol), 3):
-    yAvg.append(chainedcol[i])
-
-plt.plot(x, yMax)
-plt.plot(x, yMin)
-plt.plot(x, yAvg)
-plt.title("Chained")
-plt.xlabel("Percents")
-plt.ylabel("Collisions")
-plt.legend(["Max","Min","Avg"],loc = 2)
-plt.show()
-
-iacoll = iaresult[0]
+iacol = iaresult[0]
 iaisp = iaresult[1]
-x = perc
-yMax = []
-for i in range(0,len(iacoll),3):
-    yMax.append(iacoll[i])
-yMin = []
-for i in range(1,len(iacoll),3):
-    yMin.append(iacoll[i])
-yAvg = []
-for i in range(2,len(iacoll),3):
-    yAvg.append(iacoll[i])
 
-plt.plot(x,yMax)
-plt.plot(x,yMin)
-plt.plot(x,yAvg)
-plt.xlabel("Percents")
-plt.ylabel("Collisions")
-plt.title("Indirizzamento Aperto(collisioni)")
-plt.legend(["Max","Min","Avg"],loc = 2)
+# Plot collisions for chained hash
+xAxis = perc
+yMax = [i[0] for i in chainedcol]
+yMin = [i[1] for i in chainedcol]
+yAvg = [i[2] for i in chainedcol]
+
+plt.plot(xAxis, yMax)
+plt.plot(xAxis, yMin)
+plt.plot(xAxis, yAvg)
+
+plt.xlabel('Percents')
+plt.ylabel('Collisions')
+plt.legend(['Max', 'Min', 'Avg'], loc=2)
+plt.title("Chained")
 plt.show()
 
-yMax = []
-for i in range(0,len(iaisp),3):
-    yMax.append(iaisp[i])
-yMin = []
-for i in range(1,len(iaisp),3):
-    yMin.append(iaisp[i])
-yAvg = []
-for i in range(2,len(iaisp),3):
-    yAvg.append(iaisp[i])
-plt.plot(x,yMax)
-plt.plot(x,yMin)
-plt.plot(x,yAvg)
-plt.xlabel("Percents")
-plt.ylabel("Ispections")
-plt.title("Indirizzamneto Aperto(ispezione)")
-plt.legend(["Max","Min","Avg"],loc = 2)
+# Plot collisions for open hash
+yMax = [i[0] for i in iacol]
+yMin = [i[1] for i in iacol]
+yAvg = [i[2] for i in iacol]
+
+plt.plot(xAxis, yMax)
+plt.plot(xAxis, yMin)
+plt.plot(xAxis, yAvg)
+
+plt.xlabel('Percents')
+plt.ylabel('Collisions')
+plt.legend(['Max', 'Min', 'Avg'], loc=2)
+plt.title("Indirizzamento aperto(collisioni)")
+plt.show()
+
+# Plot inspections for open hash
+yMax = [i[0] for i in iaisp]
+yMin = [i[1] for i in iaisp]
+yAvg = [i[2] for i in iaisp]
+
+plt.plot(xAxis, yMax)
+plt.plot(xAxis, yMin)
+plt.plot(xAxis, yAvg)
+
+plt.xlabel('Percents')
+plt.ylabel('Insepctions')
+plt.legend(['Max', 'Min', 'Avg'], loc=2)
+plt.title("Indirizzamento aperto(ispezioni)")
 plt.show()
--- test.py
+++ test.py
@@ -1,117 +1,76 @@
 import pickle
-import IAhash
 import numpy as np
 import random
-import CChash
-
-def media(l):
-    count = 0.
-    for i in range(len(l)):
-        count += l[i]
-    return count / len(l)
+import IAhash as ho
+import CChash as hc
 
 
 def test():
     val = pickle.load(open("percents.p", "rb"))
-
     chainedr = Chained(val)
     iaopenr = Iaperto(val)
 
-    pickle.dump(chainedr,open("chained.p", "wb"))
+    pickle.dump(chainedr, open("chained.p", "wb"))
     pickle.dump(iaopenr, open("iaopen.p", "wb"))
 
 
 def Chained(val):
+    percentage = val[1]
     collist = []
     totcoll = []
-    rd = []
-    perc = []
-    for i in range(len(val[1])):
-        perc.append((val[0] * val[1][i]) / 100)
-    for i in range(len(perc)):
-        for j in range(20):
-            cc = CChash.HashC(val[0])
-            rd.append(random.randint(0, 100 * val[0]))
-            for k in rd:
-                cc.Cinsert(k)
-            collist.append(cc.numColl())
-        totcoll.append(max(collist))
-        totcoll.append(min(collist))
-        totcoll.append(media(collist))
-        collist = []
-    print "Valore di m preso:", cc.getM()
-   # print cc.tostring()
-    return totcoll
+
+    for i in percentage:
+        for j in range(0, 20):
+            rd = random.sample(range(100 * val[0]), val[0] * i / 100)
+            cc = hc.HashC(val[0])
+            for value in rd:
+                cc.Cinsert(value)
+
+            totcoll.append(cc.numColl())
+
+        maxCollisionsValue = max(totcoll)
+        minCollisionsValue = min(totcoll)
+        avgCollisionsValue = sum(totcoll) / len(totcoll)
+        cList = [maxCollisionsValue, minCollisionsValue, avgCollisionsValue]
+
+        collist.append(cList)
+        totcoll = []
+
+    return collist
 
 
 def Iaperto(val):
-    collist = []
+    m = val[0]
+    percentage = val[1]
     totcoll = []
-    isplength = []
-    isp = []
     totisp = []
-    rd = []
-    perc = []
-    for i in range(len(val[1])):
-        perc.append((val[0] * val[1][i]) / 100)
-    for i in range(len(perc)):
-        for i in range(20):
-            ia = IAhash.Hash(val[0])
-            rd.append(random.randint(0, 100 * val[0]))
-            for k in rd:
-                ia.indAinsert(k)
+    collist = []
+    isp = []
+    for j in percentage:
+        for i in range(0, 20):
+            ia = ho.Hash(m)
+            rd = random.sample(range(100 * m), m * j // 100)
+
+            for value in rd:
+                ia.indAinsert(value)
+
             collist.append(ia.getC())
-            isplength = ia.getisp()
-            if isplength != []:
-                isp.append(max(isplength))
-                isp.append(min(isplength))
-                isp.append(media(isplength))
-        totisp.append(max(isp))
-        totisp.append(min(isp))
-        totisp.append(media(isp))
+            isp.append(ia.getisp())
 
-        totcoll.append(max(collist))
-        totcoll.append(min(collist))
-        totcoll.append(media(collist))
+        maxC = max(collist)
+        minC = min(collist)
+        avgC = sum(collist) / len(collist)
+        cList = [maxC, minC, avgC]
 
-        isplength = []
+        maxI = max(max(v) for v in isp)
+        minI = min(min(v) for v in isp)
+        avgI = sum(sum(v) / len(v) for v in isp) / len(isp)
+        iList = [maxI, minI, avgI]
+
+        totcoll.append(cList)
+        totisp.append(iList)
+
         collist = []
-    print "Valore di m preso: ", ia.getM()
-    #print ia.tostring()
+        isp = []
+
     return totcoll, totisp
-
-
-# val = pickle.load(open("percents.p", "rb"))
-# Iaperto(val)
-# Chained(val)
-
-'''
-a = np.empty([2, ], dtype=list)
-a[0] = 10
-a[1] = [10, 50, 90]
-
-ia = IAhash.Hash(a[0])
-b = []
-for i in range(3):
-    b.append((a[0] * a[1][i]) / 100)
-    ia = IAhash.Hash(a[0])
-    for j in range(b[i]):
-        ia.indAinsert(random.randint(0, 100 * a[0]))
-    ia.tostring()
-    print "collisioni:", ia.getC()
-    print ia.getisp()
-    ip = ia.getisp()
-    if ip != []:
-        print "max:", max(ip), "min:", min(ip), "avg:", media(ip), "\n"
-
-print " "
-cc = CChash.HashC(a[0])
-c = []
-for i in range(3):
-    c.append((a[0] * a[1][i]) / 100)
-    cc = CChash.HashC(a[0])
-    for j in range(c[i]):
-        cc.Cinsert(random.randint(0, 100 * a[0]))
-    cc.tostring()
-    print "collisioni:", cc.numColl(), "\n"
-'''
